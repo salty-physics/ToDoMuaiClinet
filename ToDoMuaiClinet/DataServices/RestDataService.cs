51827a6e79e524197f5206e4a6f8baf0d502e34f
Michal Stachura <gritoo@gmail.com>
2023-01-01 18:52:18 +0100

    serivce costructor

---
+++ ToDoMuaiClinet/DataServices/RestDataService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ToDoMuaiClinet.Models;
+
+namespace ToDoMuaiClinet.DataServices
+{
+    public class RestDataService : IRestDataService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseAddres;
+        private readonly string _url;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        public RestDataService()
+        {
+            _httpClient = new HttpClient();
+            _baseAddres = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000" : "https://localhost:5001";
+            _url = $"{_baseAddres}/api";
+
+            _jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+        }
+
+
+        public Task AddTaskAsync(ToDo toDo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTaskAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ToDo>> GetAllToDoAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTaskAsync(ToDo toDo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
