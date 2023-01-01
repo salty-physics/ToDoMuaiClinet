d805b3dd80bfabd140ff24b63e9a86f5d0a87a4b
Michal Stachura <gritoo@gmail.com>
2023-01-01 18:20:26 +0100

    todo model

---
+++ ToDoMuaiClinet/Models/ToDo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoMuaiClinet.Models
+{
+    public class ToDo : INotifyPropertyChanged
+    {
+        int _id;
+        public int Id 
+        {
+            get => _id; 
+            set 
+            {
+                if (_id == value) return;
+
+                _id = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Id)));
+            } 
+        }
+
+        string _toDoName;
+        public string ToDoName
+        {
+            get => _toDoName;
+            set
+            {
+                if (value == _toDoName) return;
+
+                _toDoName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ToDoName)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
